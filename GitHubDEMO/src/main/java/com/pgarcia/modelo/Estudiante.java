94ed87eb11c95c349ce98d951ce44daacfd17d1e
paulgarc <paulgarc@LES005473.corp.capgemini.com>
2018-08-07 09:21:21 +0200

    Clase Estudiante creada el martes

---
+++ GitHubDEMO/src/main/java/com/pgarcia/modelo/Estudiante.java
@@ -0,0 +1,23 @@
+package com.pgarcia.modelo;
+
+public class Estudiante {
+	
+	private String nombre;
+	private String apellidos;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	
+
+}
